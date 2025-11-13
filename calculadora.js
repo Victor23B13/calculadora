eaa02bb5904a4db2a74dddab731feb51c270901f
VINÍCIUS DOS SANTOS <vinicius_santos158@estudante.sesisenai.org.br>
2025-11-13 17:05:24 -0300

    Commit de subtração

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,8 @@
-//arquivo da calculadora
+//SUBTRAÇÃO
+let nome = 'Vinicius'
+let numeroUm = 4
+let numeroDois = 2
+
+const resultado = numeroUm - numeroDois
+
+console.log(`Olá ${nome}. \nO resultado da sua subtração é de ${resultado}`)

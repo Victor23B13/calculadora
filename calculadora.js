555b0d803bbc46f722f4b068d13201fab0def466
VINÍCIUS DOS SANTOS <vinicius_santos158@estudante.sesisenai.org.br>
2025-11-17 15:31:22 -0300

    Commit de Multiplicação

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,4 @@
+//SOMA
 let nomeSoma = 'Vinicius'
 let n1 = 3
 let n2 = 3
@@ -5,3 +6,13 @@ let n2 = 3
 const resultadoSoma = n1 + n2 
 
 console.log(`Olá ${nomeSoma}.\nO resultado da sua soma é ${resultadoSoma}.`)
+
+//MULTIPLICAÇÃO
+
+let nomeMultuplicacao = 'Vinicius'
+let n3 = 3
+let n4 = 3
+
+const resultadoMultiplicacao = n3 * n4 
+
+console.log(`Olá ${nomeMultuplicacao}.\nO resultado da sua multiplicação é ${resultadoMultiplicacao}.`)

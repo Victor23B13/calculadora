76ce095e1c2f10c4006ffc64273c697bd78c8197
VINÍCIUS DOS SANTOS <vinicius_santos158@estudante.sesisenai.org.br>
2025-11-17 14:55:23 -0300

    commit de soma

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,7 @@
-//arquivo da calculadora
+let nomeSoma = 'Vinicius'
+let n1 = 3
+let n2 = 3
+
+const resultadoSoma = n1 + n2 
+
+console.log(`Olá ${nomeSoma}.\nO resultado da sua soma é ${resultadoSoma}.`)

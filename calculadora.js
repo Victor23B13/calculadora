30cea40e85be8bc6ba263544016999be86e70499
VICTOR HUGO ANTUNES CHAVES <victor_chaves@estudante.sesisenai.org.br>
2025-11-17 14:58:55 -0300

    Commit de Divisão

--- calculadora.js
+++ calculadora.js
@@ -6,3 +6,12 @@ let numeroDois = 2
 const resultado = numeroUm - numeroDois
 
 console.log(`Olá ${nome}. \nO resultado da sua subtração é de ${resultado}`)
+
+//DIVISÃO
+let nomeDivisao = 'Victor Hugo'
+let n1 = 2
+let n2 = 1
+
+const resultado2 = n1 / n2;
+
+console.log(`Olá ${nomeDivisao}. \nO resultado da sua divisão é de ${resultado2}.`)
